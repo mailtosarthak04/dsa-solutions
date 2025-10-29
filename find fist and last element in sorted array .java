d344a670df03a1681a03e097b6eb309bb1579513
mailtosarthak04 <mailtosathak04@gmail.com>
2025-10-29 12:22:27 +0530

    added file

---
+++ find fist and last element in sorted array .java
@@ -0,0 +1,37 @@
+public class Solution {
+    public int[] searchRange(int[] nums, int target) {
+        int first = findBound(nums, target, true);
+        int last = findBound(nums, target, false);
+        return new int[]{first, last};
+    }
+    private int findBound(int[] nums, int target, boolean isFirst) {
+        int left = 0, right = nums.length - 1;
+        int result = -1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] == target) {
+                result = mid;
+                if (isFirst) {
+                    right = mid - 1; // move left to find first occurrence
+                } else {
+                    left = mid + 1; // move right to find last occurrence
+                }
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return result;
+    }
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        int[] nums = {5, 7, 7, 8, 8, 10};
+        int target = 8;
+        int[] range = sol.searchRange(nums, target);
+        System.out.println("[" + range[0] + ", " + range[1] + "]");
+    }
+}
