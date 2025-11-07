61671d4c63c1e80086adf6d3c2e0119db2dc2660
mailtosarthak04 <mailtosathak04@gmail.com>
2025-11-07 09:25:02 +0530

    added file

---
+++ Is subsequence.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean isSubsequence(String s, String t) {
+        if (s.equals("")) return true; // Empty string is always a subsequence
+        int idx = 0;
+        for (int i = 0; i < t.length(); i++) {
+            if (t.charAt(i) == s.charAt(idx)) {
+                idx++;
+                if (idx == s.length()) return true;
+            }
+        }
+        return false;
+    }
+}
