ee7e13c3cffa6f4d9e2873846153bad251a0e7e2
mailtosarthak04 <mailtosathak04@gmail.com>
2025-11-12 23:13:58 +0530

    added file

---
+++ Multiply strings.java
@@ -0,0 +1,21 @@
+class Solution {
+    public String multiply(String num1, String num2) {
+        if (num1.equals("0") || num2.equals("0")) return "0";
+        int m = num1.length(), n = num2.length();
+        int[] result = new int[m + n];
+        for (int i = m - 1; i >= 0; i--) {
+            int digit1 = num1.charAt(i) - '0';
+            for (int j = n - 1; j >= 0; j--) {
+                int digit2 = num2.charAt(j) - '0';
+                int sum = result[i + j + 1] + digit1 * digit2;
+                result[i + j + 1] = sum % 10;
+                result[i + j] += sum / 10;
+            }
+        }
+        StringBuilder sb = new StringBuilder();
+        int i = 0;
+        while (i < result.length && result[i] == 0) i++; // Skip leading zeros
+        while (i < result.length) sb.append(result[i++]);
+        return sb.toString();
+    }
+}
