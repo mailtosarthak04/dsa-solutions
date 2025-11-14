250cddbaa0e2f8f70d7c306b526905eec6d4d62b
mailtosarthak04 <mailtosathak04@gmail.com>
2025-11-14 09:37:59 +0530

    added file

---
+++ Find all duplicates in an array.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class Solution {
+    public List<Integer> findDuplicates(int[] nums) {
+        Set<Integer> seen = new HashSet<>();
+        List<Integer> duplicates = new ArrayList<>();
+        for (int num : nums) {
+            if (seen.contains(num)) {
+                duplicates.add(num);
+            } else {
+                seen.add(num);
+            }
+        }
+        return duplicates;
+    }
+}
